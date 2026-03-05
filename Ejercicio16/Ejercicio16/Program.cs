0e69e73a05e600fccc802531ea9996ef4e9101b5
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:29:13 -0600

    Se terminó el proyecto 16.

--- Ejercicio16/Ejercicio16/Program.cs
+++ Ejercicio16/Ejercicio16/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.Diagnostics.Contracts;
+string contra;
+do
+{
+    Console.WriteLine("Ingrese su contraseña: ");
+    Console.WriteLine();
+    contra = Console.ReadLine();
+    Console.WriteLine();
+} while (contra != "koala");
+Console.WriteLine("Contraseña correcta");
