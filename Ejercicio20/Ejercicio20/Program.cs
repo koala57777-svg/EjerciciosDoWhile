73cacfad01000107413be9061ab742d4cf8b93f1
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 17:07:22 -0600

    Se terminó el proyecto 20.

--- Ejercicio20/Ejercicio20/Program.cs
+++ Ejercicio20/Ejercicio20/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿double numeroIngresado = 0;
+double suma = 0;
+int contador = 0;
+
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Ingrese un número (Ingrese 0 para mostrar resultados): ");
+    Console.WriteLine();
+    numeroIngresado = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    suma += numeroIngresado;
+    if (numeroIngresado != 0)
+    {
+        contador++;
+    }
+} while (numeroIngresado != 0);
+Console.WriteLine();
+Console.WriteLine("Los resultados de los números ingresados: ");
+Console.WriteLine();
+Console.WriteLine($"La suma total de los números: {suma}");
+Console.WriteLine();
+Console.WriteLine($"La cantidad de números que se ingresaron fueron: {contador}");
+Console.WriteLine();
+Console.WriteLine($"El promedio de los número ingresado es: {(suma/contador)}");
