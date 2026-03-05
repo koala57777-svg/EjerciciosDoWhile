721ca54e803ed504bcc79b1c23b5ad6fe269b1d7
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:34:48 -0600

    Se terminó el proyecto 17.

--- Ejercicio17/Ejercicio17/Program.cs
+++ Ejercicio17/Ejercicio17/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int contador = 0;
+int numeroIngresado = 0;
+
+do
+{
+    Console.WriteLine("Ingrese un número (Ingrese 0 para saber cuantos son pares): ");
+    Console.WriteLine();
+    numeroIngresado=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (numeroIngresado % 2 == 0 && numeroIngresado!=0)
+    {
+        contador++;
+    }
+}while (numeroIngresado!=0);
+Console.WriteLine($"La cantidad de número pares ingresados fueron: {contador}");
