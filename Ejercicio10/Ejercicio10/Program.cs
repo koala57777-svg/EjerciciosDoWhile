051d6b7e868cfd0fec0230ba466286d9a107a288
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 15:54:50 -0600

    Se terminó el proyecto 10.

--- Ejercicio10/Ejercicio10/Program.cs
+++ Ejercicio10/Ejercicio10/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int numero = 1;
+int contador = 0;
+
+do
+{
+    contador++;
+    numero++;
+    
+} while (numero<= 50);
+
+Console.WriteLine($"La cantidad de números que hay son: {contador}");
