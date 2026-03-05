37af333c8eaea4e3fc742672a694f37adb243f5a
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:49:38 -0600

    Se terminó el proyecto 18. Se aprendió a generar números "aleatorios".

--- Ejercicio18/Ejercicio18/Program.cs
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿int opcion = -1;
+
+
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Elija una opción: ");
+    Console.WriteLine();
+    Console.WriteLine("1. Mostrar saludo");
+    Console.WriteLine("2. Mostrar número aleatorio");
+    Console.WriteLine("3. Salir");
+    Console.WriteLine();
+    opcion = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Hola, ¿que tal?");
+            
+            break;
+        case 2:
+            Random rnd = new Random();
+            int numero = rnd.Next(57, 5777);
+            Console.WriteLine($"Número aleatorio: {numero}");
+            
+            break;
+        case 3:
+            break;
+        default:
+            Console.WriteLine("Opción no válida. Elija un número entre 1 y 3.");
+            
+            break;
+    }
+} while (opcion != 3);
+Console.WriteLine("Saliendo del programa...");
