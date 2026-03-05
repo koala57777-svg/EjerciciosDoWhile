ecffd92df4bd3252468630f08ff89547fa8783dc
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 15:50:04 -0600

    Se terminó el proyecto 9.

--- Ejercicio9/Ejercicio9/Program.cs
+++ Ejercicio9/Ejercicio9/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int numero = 1;
+
+do
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($" {numero} es par");
+    }
+    else
+    {
+        Console.WriteLine($" {numero} es impar");
+    }
+    Console.WriteLine();
+    numero++;
+} while (numero <= 20);
