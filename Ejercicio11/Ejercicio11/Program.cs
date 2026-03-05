fc143b62b44127a335ddee526f1648a2986d7a91
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:02:37 -0600

    Se terminó el proyecto 11.

--- Ejercicio11/Ejercicio11/Program.cs
+++ Ejercicio11/Ejercicio11/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿double suma = 0;
+int numero = 1;
+double numeroIngresado = 0;
+do
+{
+    Console.WriteLine($"Ingrese el {numero}° número: ");
+    Console.WriteLine();
+    numeroIngresado = double.Parse(Console.ReadLine());
+    suma += numeroIngresado;
+    numero++;
+    Console.WriteLine();
+} while (numero <= 5);
+Console.WriteLine($"La suma total es: {suma}");
