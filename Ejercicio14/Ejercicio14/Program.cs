8291bf08a8a838e13b8478ec6eccd87b1faf40db
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:21:20 -0600

    Se terminó el proyecto 14.

--- Ejercicio14/Ejercicio14/Program.cs
+++ Ejercicio14/Ejercicio14/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿double mayornumero=-999999999;
+double numeroIngresado = -1;
+
+do
+{
+    Console.WriteLine("Ingrese un número (Ingrese 0 para saber cual fue el mayor número): ");
+    Console.WriteLine();
+    numeroIngresado = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (numeroIngresado > mayornumero)
+    {
+        mayornumero = numeroIngresado;
+    }
+} while (numeroIngresado != 0);
+Console.WriteLine($"El mayor número ingreasdo fue: {mayornumero}");
