a3a21dfa75e42f053206cd5f1310dcb950a893a0
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:54:58 -0600

    Se terminó el proyecto 19.

--- Ejercicio19/Ejercicio19/Program.cs
+++ Ejercicio19/Ejercicio19/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine(" Ingrese un número para crear su tabla de multiplicar: ");
+Console.WriteLine();
+int numero=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($" La tabla del {numero}");
+Console.WriteLine();
+int multiplo = 1;
+do
+{
+    Console.WriteLine($" {numero} x {multiplo} = {numero * multiplo}");
+    Console.WriteLine();
+    multiplo++;
+} while (multiplo <= 10);
