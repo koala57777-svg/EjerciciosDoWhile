a0f856ac6c79298a93f4620007c187100e09f389
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:09:23 -0600

    Se terminó el proyecto 12.

--- Ejercicio12/Ejercicio12/Program.cs
+++ Ejercicio12/Ejercicio12/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿double suma = 0;
+double numeroIngresado = 0;
+
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Ingrese un número (Ingrese 0 para mostrar la suma total): ");
+    Console.WriteLine();
+    numeroIngresado = double.Parse(Console.ReadLine());
+    suma += numeroIngresado;
+} while (numeroIngresado != 0);
+Console.WriteLine();
+Console.WriteLine($"La suma tota es: {suma}");
