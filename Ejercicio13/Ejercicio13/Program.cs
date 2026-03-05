2d173d1863212f1026054d09fbe45730db61e007
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:15:25 -0600

    Se terminó el proyecto 13.

--- Ejercicio13/Ejercicio13/Program.cs
+++ Ejercicio13/Ejercicio13/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int contador = 0;
+double numeroIngresado = 0;
+
+do
+{
+    Console.WriteLine("Ingrese un número (Ingrese 0 para saber cuantos número se ingresaron): ");
+    Console.WriteLine();
+    numeroIngresado = double.Parse(Console.ReadLine());
+    contador++;
+    Console.WriteLine();
+} while (numeroIngresado != 0);
+Console.WriteLine($"La cantidad de números que se ingresaron fueron de: {contador}");
