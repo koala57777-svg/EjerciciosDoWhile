f0392de98d0748e0d7ca5ca9721c086a484ea4bf
Gerber Ramírez <koala57777@gmail.com>
2026-03-05 16:25:23 -0600

    Se terminó el proyecto 15.

--- Ejercicio15/Ejercicio15/Program.cs
+++ Ejercicio15/Ejercicio15/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿double numeroIngresado = 0;
+
+do
+{
+    Console.WriteLine("Ingrese un número positivo: ");
+    Console.WriteLine();
+    numeroIngresado = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+} while (numeroIngresado <= 0);
+Console.WriteLine("Número válido");
